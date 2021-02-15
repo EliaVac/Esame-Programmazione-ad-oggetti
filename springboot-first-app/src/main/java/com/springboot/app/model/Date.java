d50e7732426822dfe481339375b0e20e1ac473ed
Federico <Federico@DESKTOP-EN23SQ8>
2021-02-15 19:55:40 +0100

    Database

--- springboot-first-app/src/main/java/com/springboot/app/model/Date.java
+++ springboot-first-app/src/main/java/com/springboot/app/model/Date.java
@@ -1,5 +1,88 @@
 package com.springboot.app.model;
 
 public class Date {
-	private int anno;
+	private int year;
+	private int month;
+    private int day;
+    private int hour;
+    private int minute;
+	/**
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @param hour
+	 * @param minute
+	 */
+	public Date(int year, int month, int day, int hour, int minute) {
+		super();
+		this.year = year;
+		this.month = month;
+		this.day = day;
+		this.hour = hour;
+		this.minute = minute;
+	}
+	/**
+	 * @return the year
+	 */
+	public int getYear() {
+		return year;
+	}
+	/**
+	 * @param year the year to set
+	 */
+	public void setYear(int year) {
+		this.year = year;
+	}
+	/**
+	 * @return the month
+	 */
+	public int getMonth() {
+		return month;
+	}
+	/**
+	 * @param month the month to set
+	 */
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	/**
+	 * @return the day
+	 */
+	public int getDay() {
+		return day;
+	}
+	/**
+	 * @param day the day to set
+	 */
+	public void setDay(int day) {
+		this.day = day;
+	}
+	@Override
+	public String toString() {
+		return "Date: "+year+ "/" + month + "/" + day + " - hour: " + hour + ":" + minute+ ";";
+	}
+	/**
+	 * @return the hour
+	 */
+	public int getHour() {
+		return hour;
+	}
+	/**
+	 * @param hour the hour to set
+	 */
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	/**
+	 * @return the minute
+	 */
+	public int getMinute() {
+		return minute;
+	}
+	/**
+	 * @param minute the minute to set
+	 */
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
 }
--- springboot-first-app/src/main/java/com/springboot/app/util/DatabaseCraft.java
+++ springboot-first-app/src/main/java/com/springboot/app/util/DatabaseCraft.java
@@ -1,12 +1,30 @@
 package com.springboot.app.util;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+
+import json.org.JSONArray;
 
 import com.springboot.app.model.Event;
 
 public class DatabaseCraft {
 	public ArrayList<Event> getDatabase() {
 		ArrayList<Event> database = new ArrayList<>();
+		StringBuilder content = new StringBuilder();
+
+		try {
+		URL url = new URL("https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=JG0IW5CN7ztN9QAuDWjF3PFTyetbLtoG");
+		URLConnection urlConnection = url.openConnection();
+		BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+		String line;
+		while ((line = br.readLine()) != null) {​​
+		content.append(line + "\n");
+		}​​
+		br.close();
+		JSONArray databaseArray = new JSONArray(line);
 		return database;
 	}
 }
